fe4bf68edbdd6408d2b46a2e0daa6872dd8343d5
skipdaily <jimmyturlack@gmail.com>
2026-05-03 09:41:41 -0700

    Expand practice data lists

--- src/data/math.ts
+++ src/data/math.ts
@@ -4,15 +4,27 @@ const mapToGameItem = (list: string[]): GameItem[] => list.map(word => ({ prompt
 
 export const MATH_LIST_EASY = mapToGameItem([
   "1/2", "1/4", "3/4", "0.5", "0.1", "1.5", "2.0", "line", "dot", "ray",
-  "cube", "cone", "area", "math", "add", "sum", "box", "half"
+  "cube", "cone", "area", "math", "add", "sum", "box", "half",
+  "one", "two", "ten", "zero", "plus", "minus", "count", "total", "equal", "even",
+  "odd", "pair", "side", "edge", "face", "point", "shape", "round", "flat", "tall",
+  "short", "wide", "long", "more", "less", "same", "part", "whole", "1/3", "2/3",
+  "1/5", "2/5", "0.25", "0.75"
 ]);
 
 export const MATH_LIST_MEDIUM = mapToGameItem([
   "1/3", "2/5", "3/8", "0.25", "0.75", "1.25", "3.14", "angle", "circle", "square",
-  "vertex", "side", "base", "prism", "shape", "equal", "minus"
+  "vertex", "side", "base", "prism", "shape", "equal", "minus",
+  "divide", "times", "factor", "product", "difference", "greater", "lesser", "parallel", "pattern", "number",
+  "digit", "decimal", "fraction", "graph", "chart", "tally", "length", "height", "width", "weight",
+  "meter", "cent", "penny", "nickel", "dime", "dollar", "minute", "second", "hour", "calendar",
+  "2/7", "4/5", "5/8", "7/10", "0.125", "2.25", "4.5"
 ]);
 
 export const MATH_LIST_HARD = mapToGameItem([
   "3.1415", "5/16", "7/8", "0.333", "decimal", "fraction", "geometry", "polygon",
-  "triangle", "rectangle", "cylinder", "perimeter", "volume", "diameter", "radius", "equation"
+  "triangle", "rectangle", "cylinder", "perimeter", "volume", "diameter", "radius", "equation",
+  "quotient", "remainder", "multiple", "numerator", "denominator", "coordinate", "quadrant", "symmetry", "variable", "evaluate",
+  "estimate", "compare", "calculate", "equivalent", "improper", "mixed number", "acute angle", "right angle", "obtuse angle", "line segment",
+  "ray", "endpoint", "intersect", "congruent", "parallel lines", "perpendicular", "circumference", "surface area", "place value", "expanded form",
+  "standard form", "word form", "probability", "data table", "bar graph", "line plot", "mean", "median", "mode", "range"
 ]);
